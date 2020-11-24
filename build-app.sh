9089216b80953bc1ed68519e9391ea987fa94483
Jason Gullifer <jason.gullifer@gmail.com>
2020-11-24 15:50:02 -0500

    using new version of LEP

--- build-app.sh
+++ build-app.sh
@@ -3,8 +3,8 @@
 git clone https://github.com/jasongullifer/plumber-api-docker.git
 git clone https://github.com/jasongullifer/shiny-frontend-docker.git
 
-cp languageEntropyPredictor_0.0.0.9000.tar.gz plumber-api-docker/
-cp languageEntropyPredictor_0.0.0.9000.tar.gz shiny-frontend-docker/
+cp languageEntropyPredictor_0.0.1.0000.tar.gz plumber-api-docker/
+cp languageEntropyPredictor_0.0.1.0000.tar.gz shiny-frontend-docker/
 
 (cd plumber-api-docker && sh build_docker.sh)
 (cd shiny-frontend-docker && sh build_docker.sh)
